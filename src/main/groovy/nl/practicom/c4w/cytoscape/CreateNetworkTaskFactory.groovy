2dbb5d1e7eff287758bbcc6d77445b314419bac4
Stefan Warringa <swarringa@intreq.nl>
2019-09-03 22:33:09 +0200

    Set label, shape and fill color for nodes

--- src/main/groovy/nl/practicom/c4w/cytoscape/CreateNetworkTaskFactory.groovy
+++ src/main/groovy/nl/practicom/c4w/cytoscape/CreateNetworkTaskFactory.groovy
@@ -1,10 +1,13 @@
 package nl.practicom.c4w.cytoscape
 
+import org.cytoscape.application.CyApplicationManager
+import org.cytoscape.event.CyEventHelper
 import org.cytoscape.model.CyNetworkFactory
 import org.cytoscape.model.CyNetworkManager
 import org.cytoscape.session.CyNetworkNaming
 import org.cytoscape.view.model.CyNetworkViewFactory
 import org.cytoscape.view.model.CyNetworkViewManager
+import org.cytoscape.view.vizmap.VisualMappingManager
 import org.cytoscape.work.AbstractTaskFactory
 import org.cytoscape.work.TaskIterator
 
@@ -14,22 +17,35 @@ class CreateNetworkTaskFactory extends AbstractTaskFactory {
   private final CyNetworkNaming namingUtil
   private final CyNetworkViewFactory cnvf
   private final CyNetworkViewManager networkViewManager
+  private final CyApplicationManager cam
+  private final VisualMappingManager vmm
+  private final CyEventHelper eh
 
   CreateNetworkTaskFactory(
-    final CyNetworkManager netMgr,
-    final CyNetworkNaming namingUtil,
+    final CyNetworkManager cnm,
+    final CyNetworkNaming cnn,
     final CyNetworkFactory cnf,
     final CyNetworkViewFactory cnvf,
-    final CyNetworkViewManager networkViewManager
+    final CyNetworkViewManager nvm,
+    final CyApplicationManager cam,
+    final VisualMappingManager vmm,
+    final CyEventHelper eh
   ){
-    this.netMgr = netMgr
-    this.namingUtil = namingUtil
+    this.netMgr = cnm
+    this.namingUtil = cnn
     this.cnf = cnf
     this.cnvf = cnvf
-    this.networkViewManager = networkViewManager
+    this.networkViewManager = nvm
+    this.cam = cam
+    this.vmm = vmm
+    this.eh = eh
   }
 
   TaskIterator createTaskIterator(){
-    return new TaskIterator(new CreateNetworkTask(netMgr, namingUtil, cnf, cnvf, networkViewManager))
+    return new TaskIterator(
+        new CreateNetworkTask(
+          netMgr, namingUtil, cnf, cnvf, networkViewManager, cam,vmm,eh
+        )
+    )
   }
 }
--- src/main/groovy/nl/practicom/c4w/cytoscape/CyActivator.groovy
+++ src/main/groovy/nl/practicom/c4w/cytoscape/CyActivator.groovy
@@ -1,11 +1,14 @@
 package nl.practicom.c4w.cytoscape
 
+import org.cytoscape.application.CyApplicationManager
+import org.cytoscape.event.CyEventHelper
 import org.cytoscape.model.CyNetworkFactory
 import org.cytoscape.model.CyNetworkManager
 import org.cytoscape.service.util.AbstractCyActivator
 import org.cytoscape.session.CyNetworkNaming
 import org.cytoscape.view.model.CyNetworkViewFactory
 import org.cytoscape.view.model.CyNetworkViewManager
+import org.cytoscape.view.vizmap.VisualMappingManager
 import org.cytoscape.work.TaskFactory
 import org.osgi.framework.BundleContext
 
@@ -17,18 +20,26 @@ class CyActivator extends AbstractCyActivator {
 
     void start(BundleContext bc) {
 
-        CyNetworkManager cyNetworkManagerServiceRef = getService(bc,CyNetworkManager.class)
-        CyNetworkNaming cyNetworkNamingServiceRef = getService(bc,CyNetworkNaming.class)
-        CyNetworkFactory cyNetworkFactoryServiceRef = getService(bc,CyNetworkFactory.class)
-        CyNetworkViewFactory cyNetworkViewFactoryServiceRef = getService(bc,CyNetworkViewFactory.class)
-        CyNetworkViewManager cyNetworkViewManagerServiceRef = getService(bc,CyNetworkViewManager.class)
+        final CyNetworkManager cyNetworkManagerServiceRef = getService(bc,CyNetworkManager.class)
+        final CyNetworkNaming cyNetworkNamingServiceRef = getService(bc,CyNetworkNaming.class)
+        final CyNetworkFactory cyNetworkFactoryServiceRef = getService(bc,CyNetworkFactory.class)
+        final CyNetworkViewFactory cyNetworkViewFactoryServiceRef = getService(bc,CyNetworkViewFactory.class)
+        final CyNetworkViewManager cyNetworkViewManagerServiceRef = getService(bc,CyNetworkViewManager.class)
+
+        final CyApplicationManager cyApplicationManagerServiceRef = getService(bc,CyApplicationManager.class);
+        final VisualMappingManager vmmServiceRef = getService(bc,VisualMappingManager.class)
+        final CyEventHelper cyEventHelper = getService(bc, CyEventHelper.class)
+
 
         CreateNetworkTaskFactory createNetworkTaskFactory = new CreateNetworkTaskFactory(
           cyNetworkManagerServiceRef,
           cyNetworkNamingServiceRef,
           cyNetworkFactoryServiceRef,
           cyNetworkViewFactoryServiceRef,
-          cyNetworkViewManagerServiceRef
+          cyNetworkViewManagerServiceRef,
+          cyApplicationManagerServiceRef,
+          vmmServiceRef,
+          cyEventHelper
         )
 
         Properties props = new Properties()
